a497a778554d60f38e6e275e3c53079699946cea
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2023-04-19 02:58:53 +0200

    Initial Commit

---
+++ MemoSimon/MemoSimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoSimonApp.swift
+//  MemoSimon
+//
+//  Created by Thomas Carlier on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoSimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
